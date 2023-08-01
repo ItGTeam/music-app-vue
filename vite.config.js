ad18ea3b3ab81ec21737baa8d75feb5e7871f3de
ItGTeam <itgfreelancers@gmail.com>
2023-08-01 11:18:27 +0330

    Auth Added

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 4124 // Change this to your desired port number
+  },
   plugins: [
     vue(),
   ],
