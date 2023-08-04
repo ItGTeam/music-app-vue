8d2fc9896f2d42af5b27b8836ac01175fed1535f
ItGTeam <itgfreelancers@gmail.com>
2023-08-04 23:34:29 +0330

    upload first Part

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import "firebase/auth";
 import "firebase/firestore";
+import 'firebase/storage';
 
 
 // Your web app's Firebase configuration
@@ -11,14 +12,16 @@ const firebaseConfig = {
     storageBucket: "music-app-d885f.appspot.com",
     messagingSenderId: "902714950487",
     appId: "1:902714950487:web:03c0561d96d7f565307412"
-};
+  };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
 const db = firebase.firestore();
+const storage = firebase.storage();
 
 const usersCollection = db.collection("users");
+const songsCollection = db.collection('songs');
 
-export { auth, db, usersCollection };
+export { auth, db, usersCollection,storage,songsCollection };
